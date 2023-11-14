a37550ef5a521a8e7d277152bb6aaa1a39f7336b
Jerry Okolo <jerrycreative@live.co.uk>
2023-11-14 17:17:33 +0100

    more progress; clean up files

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,4 +1,5 @@
 use hex_literal::hex;
+use substreams::scalar::{BigDecimal, BigInt};
 
 // Contracts
 pub const DEXCANDLES_FACTORY: [u8; 20] = hex!("DC8d77b69155c7E68A95a4fb0f06a71FF90B943a");
@@ -12,3 +13,10 @@ pub const CANDLESTICK_PERIODS: [i32; 6] = [
     24 * 60 * 60,     // 1d
     7 * 24 * 60 * 60, // 1w
 ];
+
+pub const BIG_DECIMAL_1E6: BigDecimal = BigDecimal::from(1_000_000);
+pub const BIG_DECIMAL_1E10: BigDecimal = BigDecimal::from(10_000_000_000);
+pub const BIG_DECIMAL_1E12: BigDecimal = BigDecimal::from(1_000_000_000_000);
+pub const BIG_DECIMAL_1E18: BigDecimal = BigDecimal::from(1_000_000_000_000_000_000);
+pub const BIG_DECIMAL_ZERO: BigDecimal = BigDecimal::from(0);
+pub const BIG_DECIMAL_ONE: BigDecimal = BigDecimal::from(1);
